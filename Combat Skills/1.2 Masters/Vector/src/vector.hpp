81b20ca2cfed32d5f7cd4647d94b42722f86544c
Tatev <tsrapivonyan@gmail.com>
2025-03-09 17:35:21 +0400

    vector update

---
+++ Combat Skills/1.2 Masters/Vector/src/vector.hpp
@@ -0,0 +1,46 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <iostream>
+#include <initializer_list>
+
+class vector
+{
+public:
+	vector();
+	vector(size_t);
+	vector(const std::initializer_list<int>&);
+	vector(const vector&);
+	vector(vector&&) noexcept;
+	vector& operator=(const vector&);
+	vector& operator=(vector&&) noexcept;
+	~vector();
+
+public:
+	int& operator[](size_t);
+	const int& operator[](size_t) const;
+	int& at(size_t);
+	const int& at(size_t) const;
+	const size_t size() const;
+	const size_t capacity() const;
+	bool is_empty() const;
+	void push_back(int);
+	void insert(size_t, int);
+	void pop_back();
+	void erase(size_t);
+	void clear() noexcept;
+	void resize(size_t);
+	void reserve(size_t);
+	void swap(vector&) noexcept;
+
+private: 
+	void copy(const vector&);
+	void index_check(size_t) const;
+
+private:
+	size_t m_capacity;
+	size_t m_size;
+	int* m_array;
+};
+
+#endif //VECTOR_HPP
